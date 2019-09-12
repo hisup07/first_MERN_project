d096e67a7ca135801cb9984572e2eb97557a2ba7
Supriyo Mondal <hisupriyooo@gmail.com>
2019-09-12 13:39:54 +0530

    'profile.'

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
+connectDB();
+//init middleware
+app.use(express.json({ extended: false }));
 
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("api running");
 });
+
+//defne route
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/post", require("./routes/api/post"));
+app.use("/api/auth", require("./routes/api/auth"));
 
 app.listen(PORT, () => console.log(`server started at ${PORT}`));
